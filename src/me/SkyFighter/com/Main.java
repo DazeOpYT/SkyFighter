8ef93e86a0b3f50589ac4fa20caecc9970ceaa08
BuildTools <unconfigured@null.spigotmc.org>
2017-12-30 13:52:27 +0100

    Hello

--- src/me/SkyFighter/com/Main.java
+++ src/me/SkyFighter/com/Main.java
@@ -1,6 +1,9 @@
 package me.SkyFighter.com;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.SkyFighter.com.Commands.SkyFighter;
 
 public class Main extends JavaPlugin
 {
@@ -18,7 +21,12 @@ public class Main extends JavaPlugin
 	
 	private void setup()
 	{
-		
+		registerCommands();
+	}
+	
+	private void registerCommands()
+	{
+		getCommand("skyfighter").setExecutor(new SkyFighter());
 	}
 	
 }
